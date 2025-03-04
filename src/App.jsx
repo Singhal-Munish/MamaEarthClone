9adce050eef5ab647c11c7f228c18d03e549c82c
Singhal-Munish <gupta.munish2k@gmail.com>
2025-03-04 23:08:04 +0530

    deploy MamaEartClone to Github

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './App.css';
-import {Routes, Route} from 'react-router-dom';
+// import {Routes, Route} from 'react-router-dom';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
@@ -19,13 +19,18 @@ function App() {
       <expInfoContext.Provider value={ {moreBtn, setMoreBtn} }>
         <InfoBar />
         <NavigationBar />
-        <Routes>
+        {/* <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/about" element={<About />}/>
           <Route path="/projects" element={<Projects />}/>
           <Route path="/contact" element={<Contact />}/>
           <Route path="*" element={<NoMatch />}/>
-        </Routes>      
+        </Routes>       */}
+        <Home />
+        <About />
+        <Projects />
+        <Contact />
+        <NoMatch />
         {/* <Footer /> */}
       </expInfoContext.Provider>
     </>
